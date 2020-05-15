b3fcf5a9487534ff92c86e1e7c0cf1715b6f723b
Sizalel <ypesce@humanbooster.com>
2020-05-15 13:56:40 +0200

    ajout de la bdd ravioles

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
 try
 {
-	$db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+	$db = new PDO('mysql:host=localhost;dbname=ravioles;charset=utf8', 'root', '');
 }
 catch (Exception $e)
 {
 	die('Echec de la connexion a la base de donnees');
 }
 
-echo 'test';
+
