2ecfa9baee9dffb7c60a5fb319527c0a000ad247
Sizalel <ypesce@humanbooster.com>
2020-05-15 14:31:29 +0200

    mise à jour du fonctionnement + affichage liste raviole

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
+<link rel="stylesheet" href="style.css">
 <?php
-include 'database.php';
+try
+{
+	$db = new PDO('mysql:host=localhost;dbname=ravioles;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+	die('Echec de la connexion a la base de donnees ' . $e->getMessage ());
+}
+?>
+	<p>Recette:</p>
+<?php
+$callback = $db->query('SELECT * FROM ravioles');
+while ($data = $callback->fetch()) {
+	?>
+	<div class="grid">
+		<div class="a"><?php echo $data['title'] ?></div>
+		<div class="b"><?php echo $data['description'] ?></div>
+		<div class="c"><?php echo $data['date'] ?></div>
+		<div class="d"><img src="<?php echo $data['img_link']?>"></div>
+	</div>
+
+	<?php
+}
+
+$callback->closeCursor ();
